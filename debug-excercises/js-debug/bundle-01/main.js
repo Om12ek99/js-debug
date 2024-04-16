6d3096de6fd0cd783a3b11a3309cd90a265533ab
Omar El Kamili <omar.elkamili@gmail.com>
2024-04-16 21:38:06 +0200

    ADD. Bundle 1: esercizio 1 corretto

---
+++ debug-excercises/js-debug/bundle-01/main.js
@@ -0,0 +1,61 @@
+/*******************************************************************************
+
+    Per ognuno dei seguenti esercizi, rispondi a queste domande:
+    1- Che cosa fa questo codice?
+    2- Sono presenti errori di sintassi?
+    3- Sono presenti errori logici?
+
+    Inserisci i tuoi commenti con le risposte, sotto ciascun esercizio
+
+*******************************************************************************/
+
+
+// ESERCIZIO 1
+for (let i = 0; i > 5; i++) {
+    console.log(i);
+}
+
+// CORREZIONE
+// 1. L'esercizio rappresenta un ciclo for che dovrebbe restituire per ogni valore dell'index un valore mostrato nella console
+// 2. Non ho riconosciuto errori di sintassi
+// 3. L'errore qui sta nel fatto che come è scritto ora è un ciclo praticamente infinito.
+// per correggere basta mettere i < 5
+
+// esercizio corretto
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+
+
+
+
+// ESERCIZIO 2
+function addIfEven(num) {
+    if (num % 2 = 0) {
+        return num + 5;
+    }
+    return num;
+}
+
+
+// ESERCIZIO 3
+function loopToFive() {
+    for (let i = 0, i < 5, i++) {
+        console.log(i);
+    }
+}
+
+
+// ESERCIZIO 4 (suggerimento: ci sono 7 errori)
+function displayEvenNumbers() {
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    let evenNumbers = [];
+    for (let i = 0; i < numbers.length - 1; i++;) {
+        if (numbers % 2 = 0); {
+            evenNumbers.push(i);
+        }
+        return evenNumbers;
+    }
+}
+displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
