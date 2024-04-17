4c1073300bc267cb470d196ebff2af5101718e3b
Omar El Kamili <omar.elkamili@gmail.com>
2024-04-17 15:04:34 +0200

    debug completato

--- debug-excercises/js-debug-objects/main.js
+++ debug-excercises/js-debug-objects/main.js
@@ -84,3 +84,87 @@ console.log(dieselCars);
 
 console.log('Tutte le altre auto');
 console.log(otherCars);
+
+
+// 1.Questo codice crea un array di oggetti che rappresentano diverse auto con informazioni come produttore,
+//  modello e tipo di carburante. Successivamente, il codice filtra questo array in tre sottoarray
+// 2.ci sono errori di sintassi:
+// Nell'assegnazione di gasolineCars, l'operatore di confronto dovrebbe essere === anziché >=.
+// Nell'assegnazione di dieselCars, manca il return statement all'interno della funzione di callback.
+// 3.c'è un errore logico:
+// Nell'assegnazione di otherCars, la condizione è erronea. Attualmente, è formulata come "il tipo di carburante 
+// non è benzina O il tipo di carburante non è diesel", che è sempre vero per qualsiasi auto, poiché non può essere
+//  entrambi contemporaneamente benzina e diesel. La corretta condizione dovrebbe essere "il tipo di carburante 
+//  non è benzina E il tipo di carburante non è diesel".
+
+
+// esercizio corretto
+const cars = [
+    {
+        manufacturer: 'Ford',
+        model: 'Fiesta',
+        type: 'diesel'
+    },
+    {
+        manufacturer: 'Audi',
+        model: 'A1',
+        type: 'benzina'
+    },
+    {
+        manufacturer: 'Volkswagen',
+        model: 'Golf',
+        type: 'Benzina'
+    },
+    {
+        manufacturer: 'Fiat',
+        model: 'Panda',
+        type: 'metano'
+    },
+    {
+        manufacturer: 'Fiat',
+        model: 'Multipla',
+        type: 'GPL'
+    },
+    {
+        manufacturer: 'Tesla',
+        model: 'Model 3',
+        type: 'elettrico'
+    },
+    {
+        manufacturer: 'Volkswagen',
+        model: 'Polo',
+        type: 'benzina'
+    },
+    {
+        manufacturer: 'Ford',
+        model: 'Kuga',
+        type: 'Diesel'
+    },
+    {
+        manufacturer: 'Seat',
+        model: 'Ibiza',
+        type: 'metano'
+    },
+    {
+        manufacturer: 'Audi',
+        model: 'R8',
+        type: 'Benzina'
+    },
+];
+
+const gasolineCars = cars.filter(auto => auto.type === 'benzina');
+
+const dieselCars = cars.filter(auto => auto.type === 'diesel');
+
+const otherCars = cars.filter(auto => auto.type !== 'benzina' && auto.type !== 'diesel');
+
+console.log('Auto a benzina');
+console.log('*******************************');
+console.log(gasolineCars);
+
+console.log('Auto a diesel');
+console.log('*******************************');
+console.log(dieselCars);
+
+console.log('Tutte le altre auto');
+console.log(otherCars);
